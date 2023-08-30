2f27616251af583d389e8ec2d4ff9cdac3f3dc93
JulianaGSouza <juulianags@gmail.com>
2023-08-30 15:11:47 -0400

    Init Repo

---
+++ solution/SportsHallPlannerTests.java
@@ -0,0 +1,19 @@
+package delft;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.Collections.*;
+import static org.assertj.core.api.Assertions.*;
+import static delft.Field.*;
+import static delft.Property.*;
+import static delft.SportsHallPlanner.planHalls;
+
+class SportsHallPlannerTests {
+
+   
+}
